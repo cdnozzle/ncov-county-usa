30fe81a0d9cdaf560256c374d2093a7eb72d70eb
cdnozzle <58290442+cdnozzle@users.noreply.github.com>
2020-03-29 00:00:34 -0400

    Add files via upload

--- ncov_state_nyt.py
+++ ncov_state_nyt.py
@@ -5,23 +5,16 @@ Created on Sat Mar 28 11:55:34 2020
 n-cov tracker from public sources
 @author: cdnozzle
 """
-
-import quandl
 import pandas as pd
-import yfinance as yf
 import streamlit as st
 import cufflinks as cf
-import numpy as np
-import plotly.express as px
 
 @st.cache()
 def get_data_link(link):
     data = pd.read_csv(link)
     return data
-
-
 '''
-# 2019-nCoV tracker by county 
+# 2019-nCoV tracker by county in the US
 
 ## Data from github repository Of [New York times](https://github.com/nytimes/covid-19-data)
 '''
@@ -32,6 +25,10 @@ data['County, State'] = data['county'] +' ,' +data['state']
 data.index = pd.MultiIndex.from_frame(data[['date','County, State']])
 #st.write(data.head(n=10))
 confirmed = data['cases'].unstack()
+
+last_value=confirmed.iloc[-1].sort_values(ascending=False)
+st.markdown('## Counties with most cases')
+st.write(last_value.head(n=10))
 
 
 select = st.sidebar.multiselect('Select counties', confirmed.columns.values)
@@ -44,7 +41,7 @@ st.markdown('## Daily New cases')
 ax1= confirmed[select].diff().iplot(asFigure=True)
 st.plotly_chart(ax1)
 
-st.markdown('## county growth rates vs total cases')
+st.markdown('## County growth rates vs total cases')
 # #country_sel = st.selectbox('Select country',confirmed.columns.values)
 
 full_scat=pd.DataFrame()
