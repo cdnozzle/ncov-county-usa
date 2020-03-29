6eb8a0ea2303b797ca2c465b13b1b3b6cb2d1f10
cdnozzle <58290442+cdnozzle@users.noreply.github.com>
2020-03-29 12:58:57 -0400

    Add files via upload

--- ncov_state_nyt.py
+++ ncov_state_nyt.py
@@ -8,6 +8,8 @@ n-cov tracker from public sources
 import pandas as pd
 import streamlit as st
 import cufflinks as cf
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 @st.cache()
 def get_data_link(link):
@@ -28,7 +30,7 @@ confirmed = data['cases'].unstack()
 
 last_value=confirmed.iloc[-1].sort_values(ascending=False)
 st.markdown('## Counties with most cases')
-st.write(last_value.head(n=10))
+st.write(last_value.head(n=20))
 
 
 select = st.sidebar.multiselect('Select counties', confirmed.columns.values)
@@ -45,14 +47,25 @@ st.markdown('## County growth rates vs total cases')
 # #country_sel = st.selectbox('Select country',confirmed.columns.values)
 
 full_scat=pd.DataFrame()
+reg_val  =pd.DataFrame()
 for i in select:
     scat=pd.concat([confirmed[i],confirmed[i].diff()],axis=1)
     scat.columns = ['Level','Change']
     scat.fillna(0,inplace=True)
+    scat = scat[~(scat == 0).any(axis=1)]
+    # Log Log regression
+    X = np.log10(scat['Level']).values
+    Y = np.log10(scat['Change']).values
+    reg =LinearRegression().fit(X.reshape(-1,1),Y)
+    reg_val.loc[i,'Coef'] = reg.coef_[0]
+    
     scat['Country']=i
     full_scat =pd.concat([full_scat,scat])
+
 
 #st.write(scat)
 ax2 =full_scat.iplot(asFigure=True,x='Level',y='Change',mode='markers',logx=True,logy=True,categories='Country',xTitle='Total Cases',
                       yTitle='New Cases')
 st.plotly_chart(ax2)
+st.markdown('## Regression Log (Change) vs Log( Level)')
+st.write(reg_val)
